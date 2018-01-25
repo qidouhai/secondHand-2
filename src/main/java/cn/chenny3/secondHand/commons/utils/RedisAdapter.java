d5adaae439b170fb1be93a74dd539d8d68160c77
csccoder <admin@chenny3.cn>
2018-01-25 16:11:31 +0800

    增加RedisAdapter类的操作类型

--- src/main/java/cn/chenny3/secondHand/commons/utils/RedisAdapter.java
+++ src/main/java/cn/chenny3/secondHand/commons/utils/RedisAdapter.java
@@ -1,7 +1,10 @@
 package cn.chenny3.secondHand.commons.utils;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,8 +12,41 @@ public class RedisAdapter {
     @Autowired
     private StringRedisTemplate redisTemplate;
 
-    public Long sadd(String key,String value){
-        return redisTemplate.opsForSet().add(key,value);
+    private SetOperations<String, String> getOpsForSet() {
+        return redisTemplate.opsForSet();
     }
+
+    private ValueOperations<String, String> getOpsForValue() {
+        return redisTemplate.opsForValue();
+    }
+
+    public Long sadd(String key, String value) {
+        return getOpsForSet().add(key, value);
+    }
+
+    public Long incr(String key) {
+        return getOpsForValue().increment(key, 1);
+    }
+
+    public Long incrBy(String key, int delta) {
+        return getOpsForValue().increment(key, delta);
+    }
+
+    public Long decr(String key) {
+        return getOpsForValue().increment(key, -1);
+    }
+
+    public Long decrBy(String key, int delta) {
+        return getOpsForValue().increment(key, delta);
+    }
+
+    public String get(String key) {
+        return getOpsForValue().get(key);
+    }
+
+    public void del(String key) {
+        redisTemplate.delete(key);
+    }
+
 
 }
